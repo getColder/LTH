3c4c4ddc643b0335d7b1fde16cf92de51ed4a862
Jiff <597368048@qq.com>
2022-04-18 14:39:05 +0800

    test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,9 +7,9 @@ window.onload = () => {
         const humanlity = document.getElementById("humanlity-data");
         const fetchTime = document.getElementById("time-fetch");
         console.log(res);
-        light.innerHTML = '光照' + res.data.light;
-        temper.innerHTML = '温度' + res.data.temper;
-        humanlity.innerHTML = '湿度' + res.data.humanlity;
-        fetchTime.innerHTML = '采集时间:' + new Date(...res.data.time).toLocaleString();
+        light.innerHTML = '光照' + res.data.Data.S;
+        temper.innerHTML = '温度' + res.data.T;
+        humanlity.innerHTML = '湿度' + res.data.H;
+        fetchTime.innerHTML = '采集时间:' + new Date(...res.data.Time).toLocaleString();
     })
 }
